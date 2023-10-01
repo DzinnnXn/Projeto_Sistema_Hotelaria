89e998da727ca5ff375c7e59e3a6f2648c7fd512
DzinnnXn <andresilvax444@gmail.com>
2023-10-01 14:12:11 -0300

    correção de erros

--- algo.py
+++ algo.py
@@ -1,121 +1,98 @@
-from classes import *
 import os
+from classes import *
 
 def pausar():
     os.system("pause")
-    
+
 def cls():
     os.system("cls")
-    
+
 def main():
     hotel = Hotel("Ibis", "Jundiaí - SP", "987654321")
-    quartos = [
-        {"tipo": "Deluxe", "qtd": 2, "capacidade": 4, "valor": 500, "desc": "Quarto de luxo: Quarto com varanda, cama de casal e hidromassagem"},
-        {"tipo": "Casal", "qtd": 4, "capacidade": 2, "valor": 300, "desc": "Quarto de casal: Quarto com cama de casal"},
-        {"tipo": "Solteiro", "qtd": 8, "capacidade": 1, "valor": 150, "desc": "Quarto de solteiro: Quarto com cama de solteiro"}
-    ]
 
-    s = 0 
+    s = 0
     while s == 0:
-        cls()
-        print("---MENU---")
-        print("01 - CADASTRAR CLIENTE")
-        print("02 - DISPONIBILIDADE DE QUARTOS")
-        print("03 - RESERVAR QUARTO")
-        print("04 - CANCELAR RESERVA")
-        print("05 - LISTAR CLIENTES")
-        print("06 - LISTAR RESERVAS")
-        print("00 - SAIR")
-        print("--------")
-        menu = int(input(">> "))
-        match menu:
-            case 1:
-                cls()
-                print("--------------------------")
-                print("    CADASTRO DE CLIENTE   ")
-                print("--------------------------")
-                print("INFORME OS SEUS DADOS")
-                id = len(hotel.cliente) + 1  
-                nome = input("Nome - ")
-                cpf = int(input("CPF - "))
-                tel = int(input("Telefone - "))                    
-                hotel.cadastrarCliente(id, nome, cpf, tel)
-                print("CLIENTE CADASTRADO")
-                print("--------")
-                pausar()
-            
-            case 2:
-                cls()
-                print("------------------------")
-                print("    QUARTO DELUXE     ")
-                print("------------------------")
-                print("Quantidade:", quartos[0]["qtd"])
-                print("Capacidade:", quartos[0]["capacidade"])
-                print("Valor:", quartos[0]["valor"])
-                print("Descrição:", quartos[0]["desc"])
-                print("")
-                print("------------------------")
-                print("    QUARTO DE CASAL     ")
-                print("------------------------")
-                print("Quantidade:", quartos[1]["qtd"])
-                print("Capacidade:", quartos[1]["capacidade"])
-                print("Valor:", quartos[1]["valor"])
-                print("Descrição:", quartos[1]["desc"])
-                print("")
-                print("------------------------")
-                print("    QUARTO SOLTEIRO     ")
-                print("------------------------")
-                print("Quantidade:", quartos[2]["qtd"])
-                print("Capacidade:", quartos[2]["capacidade"])
-                print("Valor:", quartos[2]["valor"])
-                print("Descrição:", quartos[2]["desc"])
-                pausar()
-                
-            case 3:
-                cls()
-                print("--- RESERVAR QUARTO ---")
-                print("Escolha o tipo de quarto:")
-                print("1 - Quarto Deluxe")
-                print("2 - Quarto de Casal")
-                print("3 - Quarto Solteiro")
-                tipo_quarto = int(input(">> "))
-                if tipo_quarto >= 1 and tipo_quarto <= 3:
+        try:
+            cls()
+            print("---MENU---")
+            print("01 - CADASTRAR CLIENTE")
+            print("02 - DISPONIBILIDADE DE QUARTOS")
+            print("03 - RESERVAR QUARTO")
+            print("04 - CANCELAR RESERVA")
+            print("05 - LISTAR CLIENTES")
+            print("06 - LISTAR RESERVAS")
+            print("00 - SAIR")
+            print("--------")
+            menu = int(input(">> "))
+            match menu:
+                case 1:
+                    cls()
+                    print("--------------------------")
+                    print("    CADASTRO DE CLIENTE   ")
+                    print("--------------------------")
+                    print("INFORME OS SEUS DADOS")
+                    id = len(hotel.clientes) + 1
+                    nome = input("Nome - ")
+                    cpf = int(input("CPF - "))
+                    tel = int(input("Telefone - "))
+                    hotel.cadastrarCliente(id, nome, cpf, tel)
+                    print("CLIENTE CADASTRADO")
+                    print("--------")
+                    pausar()
+
+                case 2:
+                    cls()
+                    hotel.disponibilidadeQuarto()
+                    pausar()
+                    
+                case 3:
+                    cls()
+                    print("--- RESERVAR QUARTO ---")
+                    print("Escolha o tipo de quarto:")
+                    print("1 - Quarto Deluxe")
+                    print("2 - Quarto de Casal")
+                    print("3 - Quarto Solteiro")
+                    tipo_quarto = input(">> ")
+                    if tipo_quarto in ["1", "2", "3"]:
+                        nome_cliente = input("Nome do Cliente: ")
+                        data_inicio = input("Data de Início da Reserva (dd/mm/aaaa): ")
+                        data_fim = input("Data de Fim da Reserva (dd/mm/aaaa): ")
+                        tipo_quarto = {
+                            "1": "Deluxe",
+                            "2": "Casal",
+                            "3": "Solteiro"
+                        }.get(tipo_quarto)
+
+                        if tipo_quarto:
+                            hotel.reservarQuarto(nome_cliente, data_inicio, data_fim, tipo_quarto)
+                        else:
+                            print("Opção inválida.")
+                    else:
+                        print("Opção inválida.")
+                    pausar()
+
+                case 4:
+                    cls()
+                    print("--- CANCELAR RESERVA ---")
                     nome_cliente = input("Nome do Cliente: ")
-                    data_inicio = input("Data de Início da Reserva: ")
-                    data_fim = input("Data de Fim da Reserva: ")
-                    hotel.reservarQuarto(nome_cliente, data_inicio, data_fim, quartos[tipo_quarto - 1]["tipo"])
-                else:
-                    print("Opção inválida.")
-                pausar()
-            
-            case 4:
-                cls()
-                print("--- CANCELAR RESERVA ---")
-                nome_cliente = input("Nome do Cliente: ")
-                for reserva in hotel.reservas[:]:
-                    if reserva["cliente"] == nome_cliente:
-                        tipo_quarto = reserva["tipo"]
-                        for quarto in quartos:
-                            if quarto["tipo"] == tipo_quarto:
-                                quarto["qtd"] += 1
-                        hotel.reservas.remove(reserva)
-                        print(f"Reserva para {nome_cliente} cancelada com sucesso.")
-                        break
-                else:
-                    print(f"Não foi encontrada nenhuma reserva para {nome_cliente}.")
-                pausar()
-            
-            case 5:
-                cls()
-                hotel.listarClientes()
-                pausar()
-            
-            case 6:
-                cls()
-                hotel.listarReservas()
-                pausar()
-            
-            case 0:
-                print("Saindo...")
-                pausar()
-                break
+                    hotel.cancelarReserva(nome_cliente)
+                    pausar()
+
+                case 5:
+                    cls()
+                    hotel.listarClientes()
+                    pausar()
+
+                case 6:
+                    cls()
+                    hotel.listarReservas()
+                    pausar()
+
+                case 0:
+                    print("Saindo...")
+                    pausar()
+                    break
+                
+        except Exception as erro:
+            print("Erro", erro)
+
--- classes.py
+++ classes.py
@@ -1,30 +1,55 @@
+class Quarto:
+    def __init__(self, tipo, qtd, capacidade, valor, desc):
+        self.tipo = tipo
+        self.qtd = qtd
+        self.capacidade = capacidade
+        self.valor = valor
+        self.desc = desc
+
+    def reservar(self):
+        if self.qtd > 0:
+            self.qtd -= 1
+            return True
+        else:
+            return False
+
+    def cancelarReserva(self):
+        self.qtd += 1
+
 class Hotel:
     def __init__(self, nome, endereco, cnpj):
         self.nome = nome
         self.endereco = endereco
         self.cnpj = cnpj
-        self.cliente = {}  
-        self.reserva = []
+        self.clientes = {}
+        self.reservas = []
+        self.quartos = {
+            "Deluxe": Quarto("Deluxe", 2, 4, 500, "Quarto de luxo: Quarto com varanda, cama de casal e hidromassagem"),
+            "Casal": Quarto("Casal", 4, 2, 300, "Quarto de casal: Quarto com cama de casal"),
+            "Solteiro": Quarto("Solteiro", 8, 1, 150, "Quarto de solteiro: Quarto com cama de solteiro")
+        }
 
     def cadastrarCliente(self, id, nome, cpf, tel):
         self.id = id
         self.nome = nome
         self.cpf = cpf
-        self.tel = tel        
-        self.cliente[self.id] = [self.nome, self.cpf, self.tel]
-    
+        self.tel = tel
+        self.clientes[self.id] = [self.nome, self.cpf, self.tel]
+
     def listarClientes(self):
-        for chave,valor in self.cliente.items():
+        for chave, valor in self.clientes.items():
             print(f"ID:{chave} - Nome: {valor[0]} - CPF: {valor[1]} - Telefone: {valor[2]}")
-    
-    def reservarQuarto(self, nome_cliente, data_inicio, data_fim):
-        if self.qtd > 0:
-            reserva = {"cliente": nome_cliente, "data_inicio": data_inicio, "data_fim": data_fim}
-            self.reservas.append(reserva)
-            self.qtd -= 1
-            print(f"Quarto reservado para {nome_cliente} de {data_inicio} a {data_fim}.")
+
+    def reservarQuarto(self, nome_cliente, data_inicio, data_fim, tipo_quarto):
+        quarto = self.quartos.get(tipo_quarto)
+        if quarto:
+            if quarto.reservar():
+                self.reservas.append({"cliente": nome_cliente, "data_inicio": data_inicio, "data_fim": data_fim, "tipo": tipo_quarto})
+                print(f"Quarto {tipo_quarto} reservado para {nome_cliente} de {data_inicio} a {data_fim}.")
+            else:
+                print(f"Quarto {tipo_quarto} indisponível para reserva.")
         else:
-            print("Quarto indisponível para reserva.")
+            print("Tipo de quarto inválido.")
 
     def listarReservas(self):
         if self.reservas:
@@ -35,46 +60,18 @@ class Hotel:
             print("Nenhuma reserva para este quarto.")
 
     def cancelarReserva(self, nome_cliente):
-        for reserva in self.reservas:
-            if reserva['cliente'] == nome_cliente:
+        for reserva in self.reservas[:]:
+            if reserva["cliente"] == nome_cliente:
+                tipo_quarto = reserva["tipo"]
+                quarto = self.quartos.get(tipo_quarto)
+                if quarto:
+                    quarto.cancelarReserva()
                 self.reservas.remove(reserva)
-                self.qtd += 1
                 print(f"Reserva para {nome_cliente} cancelada com sucesso.")
                 break
         else:
             print(f"Não foi encontrada nenhuma reserva para {nome_cliente}.")
 
     def disponibilidadeQuarto(self):
-        disponivel = self.qtd
-        print(f"Quarto disponível: {disponivel} de {self.qtd + len(self.reservas)}")
-
-class Quarto:
-    def __init__(self, qtd, capacidade, valor, desc):
-        self.qtd = qtd
-        self.capacidade = capacidade
-        self.valor = valor
-        self.desc = desc
-        
-    def getQtdQuarto(self):
-        return self.qtd
-    
-    def setReservaQuarto(self, qtd):
-        self.qtd = qtd
-        
-class DeluxeQuarto(Quarto):
-    qtd = 2
-    capacidade = 4
-    valor = 500
-    desc = "Quarto de luxo: Quarto com varanda, cama de casal e hidromassagem"
-
-class CasalQuarto(Quarto):
-    qtd = 4
-    capacidade = 2
-    valor = 300
-    desc = "Quarto de casal: Quarto com cama de casal"
-
-class SolteiroQuarto(Quarto):
-    qtd = 8
-    capacidade = 1
-    valor = 150
-    desc = "Quarto de solteiro: Quarto com cama de solteiro"
+        for tipo_quarto, quarto in self.quartos.items():
+            print(f"Quarto {tipo_quarto} - Disponíveis: {quarto.qtd}, Capacidade: {quarto.capacidade}")
