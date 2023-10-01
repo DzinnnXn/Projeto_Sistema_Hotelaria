f007d71222d3b6931d0e4c37e83f1a31c270511f
DzinnnXn <andresilvax444@gmail.com>
2023-10-01 12:52:21 -0300

    up

--- algo.py
+++ algo.py
@@ -1,6 +1,56 @@
 from classes import *
+import os
 
+def pausar():
+    os.system("pause")
+    
+def cls():
+    os.system("cls")
+    
 def main():
     s = 0 
     while s == 0:
-        pass
+        cls()
+        print("---MENU---")
+        print("01 - CADASTRAR CLIENTE")
+        print("02 - DISPONIBILIDADE DE QUARTOS")
+        print("03 - RESERVAR QUARTO")
+        print("04 - CANCELAR RESERVA")
+        print("05 - LISTAR CLIENTES")
+        print("06 - LISTAR RESERVAS")
+        print("00 - SAIR")
+        print("--------")
+        print("")
+        print("Qual opção deseja?")
+        menu = int(input(">> "))
+        match menu:
+            case 1:
+                cls()
+                pausar()
+            
+            case 2:
+                cls()
+                pausar()
+            
+            case 3:
+                cls()
+                pausar()
+            
+            case 4:
+                cls()
+                pausar()
+            
+            case 5:
+                cls()
+                pausar()
+            
+            case 6:
+                cls()
+                pausar()
+            
+            case 0:
+                print("Saindo...")
+                pausar()
+                break
+            
+    
--- classes.py
+++ classes.py
@@ -1,5 +1,61 @@
 class Hotel:
-    pass
+    def __init__(self, nome, endereco, cnpj):
+        self.nome = nome
+        self.endereco = endereco
+        self.cnpj = cnpj
+        self.cliente = {}
+        self.reserva = {}
 
-class Quartos:
-    pass
+    def cadastrarCliente(self, id, nome, cpf, tel):
+        self.id = id
+        self.nome = nome
+        self.cpf = cpf
+        self.tel = tel        
+        self.cliente[self.id] = [self.nome, self.cpf, self.tel]
+    
+    def listarClientes(self):
+        for chave,valor in self.cliente.items():
+            print(f"ID:{chave} - Nome: {valor[0]} - CPF: {valor[1]} - Telefone: {valor[2]}")
+    
+    def reservarQuarto(self):
+        pass
+    
+    def listarReservas(self):
+        pass
+    
+    def cancelarReserva(self):
+        pass
+
+    def disponibilidadeQuarto(self):
+        pass
+
+class Quarto:
+    def __init__(self, qtd, capacidade, valor, desc):
+        self.qtd = qtd
+        self.capacidade = capacidade
+        self.valor = valor
+        self.desc = desc
+        
+    def getQtdQuarto(self):
+        return self.qtd
+    
+    def setReservaQuarto(self, qtd):
+        self.qtd = qtd
+        
+class DeluxeQuarto(Quarto):
+    qtd = 2
+    capacidade = 4
+    valor = 500
+    desc = "Quarto de luxo: Quarto com varanda, cama de casal e hidromassagem"
+
+class CasalQuarto(Quarto):
+    qtd = 4
+    capacidade = 2
+    valor = 300
+    desc = "Quarto de casal: Quarto com cama de casal"
+
+class SolteiroQuarto(Quarto):
+    qtd = 8
+    capacidade = 1
+    valor = 150
+    desc = "Quarto de solteiro: Quarto com cama de solteiro"
